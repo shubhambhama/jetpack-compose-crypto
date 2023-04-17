53c6e8efa491012393b358f371179b9116d61a9e
Shubham Bhama <shubhambhama30@gmail.com>
2023-04-17 09:42:23 +0530

    [Fix] Release built related fixes

--- app/src/main/java/com/example/cryptoapp/network/CryptoApiResponse.kt
+++ app/src/main/java/com/example/cryptoapp/network/CryptoApiResponse.kt
@@ -1,5 +1,8 @@
 package com.example.cryptoapp.network
 
+import androidx.annotation.Keep
+
+@Keep
 data class CryptoApiResponse(val id: String, val symbol: String,
                              val name: String, val image: String, val current_price: Double,
                              val market_cap: Long, val total_volume: Double,
@@ -8,4 +11,5 @@ data class CryptoApiResponse(val id: String, val symbol: String,
                              val price_change_percentage_24h: Double,
                              val total_supply: Double, val sparkline_in_7d: LineData)
 
+@Keep
 data class LineData(val price: List<Float>)
